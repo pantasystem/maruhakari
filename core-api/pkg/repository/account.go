829ab423a9f16409ce63cfa2449d16970d95abf9
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-05-14 17:32:23 +0900

    feat: repository interface

--- core-api/pkg/repository/account.go
+++ core-api/pkg/repository/account.go
@@ -1,1 +1,17 @@
 package repository
+
+import (
+	"context"
+	"core-api/pkg/entity"
+
+	"github.com/google/uuid"
+)
+
+type AccountRepository interface {
+	FindById(ctx context.Context, id uuid.UUID) (*entity.Account, error)
+	FindByUsername(ctx context.Context, username string) (*entity.Account, error)
+	FindByToken(ctx context.Context, token uuid.UUID) (*entity.Account, error)
+	Create(ctx context.Context, account *entity.Account) (*entity.Account, error)
+	Update(ctx context.Context, account *entity.Account) (*entity.Account, error)
+	Delete(ctx context.Context, id uuid.UUID) error
+}
