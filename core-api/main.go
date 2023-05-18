e61becf65ff0a1ecdb64493eb68d2de205ff20ac
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-05-18 11:29:04 +0900

    feat

--- core-api/main.go
+++ core-api/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"core-api/pkg/config"
+	"core-api/pkg/entity"
+	"core-api/pkg/handler"
+	"core-api/pkg/module"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf("Start Server!!\n")
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	config := &config.Config{}
+	config.LoadFromEnv()
+	db, err := gorm.Open(postgres.Open(config.Dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Account{}, &entity.ContainerTemplate{}, &entity.Device{}, &entity.Food{}, &entity.FoodTemplate{}, &entity.MeasurementHistory{})
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", config.Port))
+	if err != nil {
+		panic(err)
+	}
+	m := &module.ModuleImpl{
+		DB: db,
+	}
+
+	s := handler.Setup(m)
+	go func() {
+		fmt.Printf("start gRPC server port: %v", config.Port)
+		s.Serve(listener)
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	fmt.Println("stopping gRPC server...")
+	s.GracefulStop()
 }
