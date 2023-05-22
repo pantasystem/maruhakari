de0d635cc85645471f52b6089a4205dd119d77bc
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-05-22 14:02:14 +0900

    feat:schema

--- core-api/pkg/handler/schema/measurement_history.go
+++ core-api/pkg/handler/schema/measurement_history.go
@@ -1,5 +1,7 @@
 package schema
 
+import "github.com/gin-gonic/gin"
+
 type MeasurementHistory struct {
 	Id        string  `json:"id"`
 	FoodId    string  `json:"food_id"`
@@ -11,4 +13,8 @@ type RecordHistoryRequest struct {
 	DeviceToken string  `json:"device_token"`
 	NfcUid      string  `json:"nfc_uid"`
 	Weight      float32 `json:"weight"`
+}
+
+type MeasurementHistoryController interface {
+	RecordHistory(c *gin.Context)
 }
