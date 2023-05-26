4058a6d920b6a07d306b39e442de06ba1186dfb3
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-05-26 23:36:42 +0900

    feat: ユーザ情報を入力できるようにした。

--- client/lib/pages/add_device/add_device_page.dart
+++ client/lib/pages/add_device/add_device_page.dart
@@ -36,7 +36,6 @@ class AddDevicePageState extends ConsumerState<ConsumerStatefulWidget> {
   @override
   Widget build(BuildContext context) {
     final notifier = ref.watch(addDevicePageNotifier);
-    final scanResults = ref.watch(scanResultStreamProvider);
 
     return Scaffold(
         appBar: AppBar(
@@ -53,55 +52,114 @@ class AddDevicePageState extends ConsumerState<ConsumerStatefulWidget> {
   }
 }
 
-class AddDevicePageInputServerInfoBody extends ConsumerWidget {
-  const AddDevicePageInputServerInfoBody({super.key, required this.device});
-
+class AddDevicePageInputServerInfoBody extends ConsumerStatefulWidget {
   final BluetoothDevice device;
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Column(
-      children: [
-        TextButton(
-            onPressed: () async {
+  const AddDevicePageInputServerInfoBody({super.key, required this.device});
 
-              // final targetServices = await device.services.firstWhere((element) {
-              //   log("elements:${element.map((e) => e.uuid.toString())}");
-              //   return element.any((inEl) {
-              //     log("element uuid: ${inEl.uuid}");
-              //     return inEl.uuid.toString() ==
-              //         "4426c565-997d-4902-946f-4060916183db";
-              //   });
-              // });
-              final targetServices = await device.discoverServices();
-              final service = targetServices.firstWhereOrNull((element) {
-                return element.uuid.toString() ==
-                    "4426c565-997d-4902-946f-4060916183db";
-              });
-              final c = service?.characteristics.firstWhereOrNull((element) {
-                return element.uuid.toString() ==
-                    "7a21cc0f-3845-4452-8ab6-86e035978d35";
-              });
-              if (c == null) {
-                log("characteristic is null");
-              } else {
-                log("characteristic is not null");
-              }
-              final info = const IotConnectionInfo(
-                ssid: "aaaaaa",
-                password: "test",
-                token: "token-test"
-              );
-              final json = jsonEncode(info.toJson());
-              List<int> codeUnits = [];
-              for (int i = 0; i < json.length; i++) {
-                codeUnits.add(json.codeUnitAt(i));
-              }
-              c?.write(codeUnits);
-            },
-            child: const Text("send text")),
-      ],
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return AddDevicePageInputServerInfoBodyState();
+  }
+}
+
+class AddDevicePageInputServerInfoBodyState
+    extends ConsumerState<AddDevicePageInputServerInfoBody> {
+  final inputSsidController = TextEditingController();
+  final inputPasswordController = TextEditingController();
+  final inputDeviceNameController = TextEditingController();
+
+  void _sendConnectionInfo({
+    required String ssid,
+    required String password,
+    required String token,
+  }) async {
+    final targetServices = await widget.device.discoverServices();
+    final service = targetServices.firstWhereOrNull((element) {
+      return element.uuid.toString() == "4426c565-997d-4902-946f-4060916183db";
+    });
+    final c = service?.characteristics.firstWhereOrNull((element) {
+      return element.uuid.toString() == "7a21cc0f-3845-4452-8ab6-86e035978d35";
+    });
+    if (c == null) {
+      log("characteristic is null");
+    } else {
+      log("characteristic is not null");
+    }
+    final info = IotConnectionInfo(
+      ssid: ssid,
+      password: password,
+      token: token,
     );
+    final json = jsonEncode(info.toJson());
+    List<int> codeUnits = [];
+    for (int i = 0; i < json.length; i++) {
+      codeUnits.add(json.codeUnitAt(i));
+    }
+    c?.write(codeUnits);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Wi-Fi接続情報"),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.shade300),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: inputSsidController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'SSID',
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      TextField(
+                        controller: inputPasswordController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'パスワード',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: inputDeviceNameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'デバイス名',
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    _sendConnectionInfo(ssid: inputSsidController.text, password: inputPasswordController.text, token: "token");
+                  },
+                  child: const Text("連携"),
+                )
+              ],
+            )
+          ],
+        ));
   }
 }
 
@@ -178,8 +236,7 @@ class AddDevicePageSelectDeviceBody extends ConsumerWidget {
                     return DeviceListTile(
                       onPressed: () {
                         item.state.first.then((value) {
-                          switch(value) {
-
+                          switch (value) {
                             case BluetoothDeviceState.disconnected:
                               item.connect();
                               break;
