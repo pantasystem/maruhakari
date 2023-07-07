0392ad653f8d52bf120ccea39e936219c8c7ee1e
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-07-07 14:24:50 +0900

    refactor: サーバのURLを定数として表現するようにした

--- client/lib/constants.dart
+++ client/lib/constants.dart
@@ -3,4 +3,6 @@ class AppConstants {
   const AppConstants._();
 
   static const int pollingIntervalSeconds = 5;
+
+  static const String baseUrl = "https://maruhakari.panta.systems/";
 }
--- client/lib/providers/endpoint.dart
+++ client/lib/providers/endpoint.dart
@@ -1,8 +1,9 @@
 
+import 'package:client/constants.dart';
 import 'package:client/endpoints/service.dart';
 import 'package:client/providers/repositories.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final apiClientServiceProvider = Provider((ref) {
-  return create(ref.read(authRepositoryProvider), baseUrl: "https://maruhakari.panta.systems/");
+  return create(ref.read(authRepositoryProvider), baseUrl: AppConstants.baseUrl);
 });
