ce7373174053a090b8f3552d2d6ea36ea5f5a223
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-07-11 13:37:17 +0900

    feat: 定数を追加

--- client/lib/BluetoothConstants.dart
+++ client/lib/BluetoothConstants.dart
@@ -5,4 +5,7 @@ class BluetoothConstants {
   static const String serviceUuid = "4426c565-997d-4902-946f-4060916183db";
 
   static const String connectionInfoCharacteristicUuid = "7a21cc0f-3845-4452-8ab6-86e035978d35";
+
+  static const String currentNfcAndWeightCharacteristicUuid = "fc170318-c6dd-4681-94c8-352aa763056e";
+
 }
